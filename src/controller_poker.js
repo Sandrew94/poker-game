3afe37134024d207b4747393843e67410eae3c50
Andrea santi <santi@MacBook-Air.homenet.telecomitalia.it>
2021-07-08 15:21:40 +0200

    Refactor init function and royalHand button

--- src/controller_poker.js
+++ src/controller_poker.js
@@ -12,45 +12,41 @@ const royalHand = document.querySelector(".main__container-royal");
 
 //Controller
 
-const init = function () {
-  //Create an arr of 5 elements with random numbers from 1 to 52
-  const randomNum = Array.from(Array(5), (_, i) =>
-    Math.floor(Math.random() * 51 + 1)
+//Create an arr of 5 elements with random numbers from 1 to 52
+const randomNumFunc = (arrLenght, range) => {
+  return Array.from(Array(arrLenght), (_, i) =>
+    Math.floor(Math.random() * range + 1)
   );
+};
 
+const init = function (arr) {
   //Taken ar array with random numbers, convert their to the card Matrix
-  const result = randomHand(randomNum);
+  const result = randomHand(arr);
 
   displayCard(result); //Transform the array of hand (number + seed) in card on the screen
 
-  injectValue(valueHand(randomNum).rankDescription);
-
-  valueHand(randomNum);
-
-  ///////////
-
-  const tryRandomHand = valueHand(randomNum).rankDescription;
-
-  if (tryRandomHand === "Royal Flush") {
-    console.log("Royal Flush", result);
-    return display;
-  }
+  injectValue(valueHand(arr).rankDescription);
 };
 
-btnGenerateCard.addEventListener("click", init);
+btnGenerateCard.addEventListener("click", () => init(randomNumFunc(5, 51)));
 
 /////////////
 /////////////
 /////////////
 
-//TRY TO SIMULATE ROYAL FLUSH HAND'S IN 10000000 HANDS
+//SIMULATE ROYAL FLUSH HAND'S
 
 royalHand.addEventListener("click", () => {
-  let i = 0;
+  const allTypeRoyal = [
+    [8, 9, 10, 11, 12],
+    [21, 22, 23, 24, 25],
+    [34, 35, 36, 37, 38],
+    [47, 48, 49, 50, 51],
+  ];
 
-  if (confirm("This task is strong, do you want to continue?"))
-    while (i <= 10000000) {
-      i++;
-      init();
-    }
+  allTypeRoyal.map((_, idx) => {
+    const randomVal = Math.floor(Math.random() * 4);
+
+    if (randomVal === idx) return init(Object.values(allTypeRoyal)[idx]);
+  });
 });
